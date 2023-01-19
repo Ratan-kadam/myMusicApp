202d4b3e22f5f8fd470f75c08c20f659dda5d8a3
Ratan kadam <ratan.kadam@gmail.com>
2023-01-19 15:45:27 -0500

    CSS layout for menu nav and content using grid

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,29 @@ import Search from "./pages/Search";
 import Watch from "./pages/Watch";
 import MyHome from "./pages/MyHome";
 import Nav from "./component/Nav";
+import Menu from "./component/Menu";
 
 export default function App() {
   return (
     <React.Fragment>
-      <Nav></Nav>
-        <BrowserRouter>
-          <Routes>
-          <Route path="/" element={<MyHome />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/watch/:id" element={<Watch />} />
-          </Routes>
-        </BrowserRouter>
+      <div className="container">
+        <div id="nav">
+           <Nav></Nav>
+        </div>
+        <div id="menu">
+          menu
+        </div>
+        <div id="content">
+          content
+          <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<MyHome />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/watch/:id" element={<Watch />} />
+            </Routes>
+          </BrowserRouter>
+          </div>
+        </div>
     </React.Fragment>
   );
 }
---
+++ src/component/Menu.tsx
@@ -0,0 +1,6 @@
+export default function Menu() {
+    return (<div className="menu">
+    menu
+    </div>)
+}
+

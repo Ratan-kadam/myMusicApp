cf6507702c48e1e844e6406c3f5c91c145aacc15
Ratan kadam <ratan.kadam@gmail.com>
2023-01-20 01:14:59 -0500

    play selected video

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,5 +1,18 @@
+import { useParams, useNavigate } from "react-router-dom";
+
 export default function Watch() {
+    const { id } = useParams();
     return (
-        <div>Watch</div>
+        <div>
+                <iframe
+                    width="800"
+                    height="500"
+                    src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+        </div>
     )
 }

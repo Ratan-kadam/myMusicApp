1d611c43e4ecafc73a0e8019472281ecb469c62f
Ratan kadam <ratan.kadam@gmail.com>
2023-01-20 13:11:46 -0500

    recommendation logic

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,18 +1,57 @@
+import React from "react";
+import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getVideoDetails } from "../store/reducers/getVideoDetails";
+import { getRecommendedVideos } from "../store/reducers/getRecommendedVideos";
 
 export default function Watch() {
     const { id } = useParams();
+    const dispatch = useAppDispatch();
+
+    const store = useAppSelector((state) => state);
+    const { youtubeApp } = store || {};
+    const { currentPlaying, recommendedVideos } = youtubeApp || {};
+
+    console.log("redux store:", store);
+
+    useEffect(() => {
+        dispatch(getVideoDetails(id));
+        dispatch(getRecommendedVideos(id));
+    }, [id]);
+
     return (
-        <div>
-                <iframe
-                    width="800"
-                    height="500"
-                    src={`https://www.youtube.com/embed/${id}?autoplay=1`}
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                  ></iframe>
+        <div className="watch_grid">
+        <div className="video__section">
+            <iframe
+                width="900"
+                height="500"
+                src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                ></iframe>
+        </div>
+        <div className="video_recommendation">
+         <React.Fragment>  
+            {recommendedVideos.map(video => {
+                return (<img src={video?.videoThumbnail} />)
+            })}
+            </React.Fragment> 
+        </div>
+        <div className="comment_section">
+                    <div className="watch_current_details">
+                    <div><img src={currentPlaying?.channelInfo?.image}></img></div>
+                    <div>
+                        <div> Channel: {currentPlaying?.channelInfo?.name}</div>
+                        <div>{''}</div>
+                        <div> Likes: {currentPlaying?.videoLikes}</div>
+                        <div> Views: {currentPlaying?.videoViews}</div>
+                        <div> subscribers: {currentPlaying?.channelInfo?.subscribers}</div>  
+                    </div> 
+                    </div>
+        </div>
         </div>
     )
 }

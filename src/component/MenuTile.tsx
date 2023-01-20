4d1fbab04b258f9e293466f7c22fd01aef2d36cf
Ratan kadam <ratan.kadam@gmail.com>
2023-01-20 14:13:14 -0500

    menu routing to home-page

--- src/component/MenuTile.tsx
+++ src/component/MenuTile.tsx
@@ -1,14 +1,18 @@
-import React, { Component } from "react";
+import React  from "react";
 import { MENU_ITEMS } from "../utils/allConstance";
+import { Link } from "react-router-dom";
 
 export default function MenuTile(props: any) {
     const { name, icon }  = props;
     return (<React.Fragment>
-        <div className="menu__tile">
-            <div>
-            {React.createElement(icon)}
+        <Link to={`/`}>
+            <div className="menu__tile">
+
+                <div>
+                {React.createElement(icon)}
+                </div>
+                <div>{name}</div>
             </div>
-            <div>{name}</div>
-        </div>
+        </Link>
         </React.Fragment>)
 }

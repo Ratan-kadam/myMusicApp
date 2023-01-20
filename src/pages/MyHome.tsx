c2ecf35517bb8bb55e47d4b62a5f2fd829c75c94
Ratan kadam <ratan.kadam@gmail.com>
2023-01-19 21:17:34 -0500

    Setting up Reducer

--- src/pages/MyHome.tsx
+++ src/pages/MyHome.tsx
@@ -1,4 +1,12 @@
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getHomePageVideos } from "../store/reducers/getHomePageVideos";
+
 export default function MyHome() {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(getHomePageVideos(false))
+    }, []);
     return (
         <div>Hom1e</div>
     )
